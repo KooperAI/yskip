940a11e715a5fae5fcebac19bbc86ff03e994ad4
Francisco J. Solis <siscomagma@gmail.com>
2021-03-14 17:56:35 -0600

    Added descriptions for functions

--- src/node-api.cpp
+++ src/node-api.cpp
@@ -7,6 +7,9 @@
 #include "random.h"
 #include <string>
 
+/**
+ * Class definition, which is invoked at our general Init below.
+ */
 Napi::Object Skipgram::Init(Napi::Env env, Napi::Object exports) {
     Napi::Function func = DefineClass(env, "Skipgram", {
         InstanceMethod<&Skipgram::testMethod>("testMethod"),
@@ -23,10 +26,20 @@ Napi::Object Skipgram::Init(Napi::Env env, Napi::Object exports) {
     return exports;
 }
 
+/**
+ * Constructor for the class
+ */
 Skipgram::Skipgram(const Napi::CallbackInfo& info) :
     Napi::ObjectWrap<Skipgram>(info) {
   Napi::Env env = info.Env();
   this->_instance = new yskip::Skipgram();
+}
+
+/**
+ * Destructor for the class
+ */
+Skipgram::~Skipgram() {
+  free(this->_instance);
 }
 
 /**
@@ -37,12 +50,18 @@ Napi::Value Skipgram::testMethod(const Napi::CallbackInfo& info) {
     return Napi::Boolean::New(env, true);
 }
 
+/**
+ * When calling new at the NodeJS engine this function is invoked.
+ */
 Napi::Value Skipgram::CreateNewItem(const Napi::CallbackInfo& info) {
   Napi::FunctionReference* constructor =
       info.Env().GetInstanceData<Napi::FunctionReference>();
   return constructor->New({});
 }
 
+/**
+ * Initialization for the Napi
+ */
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   Skipgram::Init(env, exports);
   return exports;
