07a7bab81c1bde7e5d02dab69bd0f9135bbbf42f
Francisco J. Solis <siscomagma@gmail.com>
2021-03-14 13:50:37 -0600

    Added C++ notation for node-api bindings

--- src/node-api.cpp
+++ src/node-api.cpp
@@ -1,19 +1,42 @@
 #include "node-api.h"
+#include "js_native_api.h"
+#include "js_native_api_types.h"
+#include "napi.h"
 
-napi_value SayHello(napi_env env, napi_callback_info info) {
-  printf("Hello\n");
-  return NULL;
+#include <string>
+
+/**
+ * Mostly based at the example at:
+ * https://www.lohika.com/node-js-n-api-implementation-and-performance-comparison
+ */
+napi_value sayHello(const Napi::CallbackInfo &info) {
+  Napi::Env env = info.Env();
+
+  // Validate argument count and type
+  if (info.Length() != 1) {
+    Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+  if (!info[0].IsString()) {
+    Napi::TypeError::New(env, "Input argument must be a string")
+      .ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  // Get input parameter
+  const Napi::String input_string = info[0].As<Napi::String>();
+  std::string result_string = Napi::String::New(env, "");
+  result_string.insert(0, "Hello, ");
+  result_string.append(input_string);
+
+  return Napi::String::New(env, result_string); 
 }
 
-napi_value Init(napi_env env, napi_value exports) {
-  napi_status status;
-
-  napi_value fn;
-  status = napi_create_function(env, NULL, 0, SayHello,NULL, &fn);
-  if (status != napi_ok) return NULL;
-
-  status = napi_set_named_property(env, exports, "sayHello", fn);
-  if (status != napi_ok) return NULL;
-
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
+  exports.Set(
+      Napi::String::New(env, "sayHello"),
+      Napi::Function::New(env, sayHello));
   return exports;
 }
+
+NODE_API_MODULE(NODE_GYP_MODULE_NAME, Init);
--- src/node-api.h
+++ src/node-api.h
@@ -1,7 +1,7 @@
 #include "js_native_api_types.h"
 #include <node.h>
+#include <napi.h>
 #include <node_api.h>
 
-napi_value SayHello(napi_env env, napi_callback_info info);
-napi_value Init(napi_env env, napi_value exports);
-NAPI_MODULE(NODE_GYP_MODULE_NAME, Init);
+napi_value SayHello(const Napi::CallbackInfo &info);
+Napi::Object Init(Napi::Env env, Napi::Object exports);
